eb531a8dae7d6ce867f5872e7fe922c4dc108185
Clayblock <57411293+Clayblock@users.noreply.github.com>
2022-02-05 14:30:12 +0100

    Update EXTRUDE_MAXLENGTH to 300

--- MKS_Robin_Nano_V1.3/Source_Code/TMC2225/Marlin/Configuration.h
+++ MKS_Robin_Nano_V1.3/Source_Code/TMC2225/Marlin/Configuration.h
@@ -717,7 +717,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 200
+#define EXTRUDE_MAXLENGTH 300
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
