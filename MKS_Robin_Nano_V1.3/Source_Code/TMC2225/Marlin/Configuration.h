968e2d58414ec0ce3cee4479a30f56d79633c507
Clayblock <57411293+Clayblock@users.noreply.github.com>
2022-02-08 01:49:34 +0100

    Update DEFAULT_MAX_FEEDRATE

--- MKS_Robin_Nano_V1.3/Source_Code/TMC2225/Marlin/Configuration.h
+++ MKS_Robin_Nano_V1.3/Source_Code/TMC2225/Marlin/Configuration.h
@@ -935,7 +935,7 @@
  * Override with M203
  *                                      X, Y, Z [, I [, J [, K]]], E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 200, 200, 4, 70 }
+#define DEFAULT_MAX_FEEDRATE          { 300, 300, 5, 70 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
