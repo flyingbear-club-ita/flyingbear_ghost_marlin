323edae50700405e5ec38bf89be4aa418cf9926c
Clayblock <57411293+Clayblock@users.noreply.github.com>
2023-08-29 21:31:02 +0200

    Merge test_branch to main

--- README.md
+++ README.md
@@ -7,38 +7,51 @@
 
 ###### Feature
 
-- Adattamento a molte configurazioni su caratterische delle stampanti Ghost 5
+- Adattamento a molte configurazioni su caratterische delle stampanti FlyingBear Ghost
 - BLTouch su tutte le configurazioni
 - Libero accesso a tutti i Source Code
+- Source Code modificato per semplificare la configurazione
+- Firmware precompilati disponibili
+
+###### Istruzioni
+
+1) Modificare il "default-envs" in Platformio.ini con quello della propria scheda (default: MKS Robin Nano V1.1 / V1.2)
+   <h1 align="left"><img src = Image/default-envs.png /></h1>
+3) Modificare la "MOTHERBOARD" in Configuration.h con quello della propria scheda (default: MKS Robin Nano V1.1 / V1.2)
+   <h1 align="left"><img src = Image/motherboard.png /></h1>
+4) Editare i parametri di base inclusi in FlyingBearGhost_ConfigFile.h con i valori personalizzati in base alla vostra configurazione
+   <h1 align="left"><img src = Image/config-file.png /></h1>
+5) Compilare attraverso il Tool "Auto Build Marlin" di Marlin Firmware, disponibile come estrnsione per Virtual Studio Code
+   <h1 align="left"><img src = Image/autobuildmarlin.png /></h1>
 
 # Versioni Disponibili
 MKS Robin Nano V1.1
-###### Versione Marlin 2.0.9.3
-###### MARLIN COLOR UI
-- [Stock](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/MKS_Robin_Nano_V1.1/Firmware/STOCK)(IN FASE DI TEST)
-- [Stock - Bltouch](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/MKS_Robin_Nano_V1.1/Firmware/STOCK_BLTOUCH)(IN FASE DI TEST)
+###### Versione Marlin 2.1.x
+###### LVGL UI
+- [Stock](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/Firmware/MKS_Robin_Nano_V1.1/STOCK)
+- [Stock - Bltouch](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/Firmware/MKS_Robin_Nano_V1.1/STOCK_BLTOUCH)
 ---
 MKS Robin Nano V1.2
-###### Versione Marlin 2.0.9.3
-###### MARLIN COLOR UI
-- [Stock](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/MKS_Robin_Nano_V1.2/Firmware/STOCK)
-- [Stock - Bltouch](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/MKS_Robin_Nano_V1.2/Firmware/STOCK_BLTOUCH)
-- [TMC2209](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/MKS_Robin_Nano_V1.2/Firmware/TMC2209)
-- [TMC2209 - Bltouch](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/MKS_Robin_Nano_V1.2/Firmware/TMC2209_BLTOUCH)
+###### Versione Marlin 2.1.x
+###### LVGL UI
+- [Stock](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/Firmware/MKS_Robin_Nano_V1.2/STOCK)
+- [Stock - Bltouch](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/Firmware/MKS_Robin_Nano_V1.2/STOCK_BLTOUCH)
+- [TMC2208](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/Firmware/MKS_Robin_Nano_V1.2/TMC2208)
+- [TMC2208 - Bltouch](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/Firmware/MKS_Robin_Nano_V1.2/TMC2208_BLTOUCH)
 ---
 MKS Robin Nano V1.3
-###### Versione Marlin 2.1
-###### MARLIN COLOR UI
-- [Stock](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/MKS_Robin_Nano_V1.3/Firmware/STOCK)
-- [TMC2209](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/MKS_Robin_Nano_V1.3/Firmware/TMC2209)
-- [Stock - Bltouch](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/MKS_Robin_Nano_V1.3/Firmware/STOCK-BLTOUCH) 
-- [TMC2209 - Bltouch](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/MKS_Robin_Nano_V1.3/Firmware/TMC2209-BLTOUCH)
+###### Versione Marlin 2.1.x
+###### LVGL UI
+- [Stock](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/Firmware/MKS_Robin_Nano_V1.3/STOCK)
+- [TMC2208](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/Firmware/MKS_Robin_Nano_V1.3/TMC2208)
+- [Stock - Bltouch](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/Firmware/MKS_Robin_Nano_V1.3/STOCK-BLTOUCH) 
+- [TMC2208 - Bltouch](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/Firmware/MKS_Robin_Nano_V1.3/TMC2208-BLTOUCH)
 ---
 MKS Robin Nano-S V1.3
-###### Versione Marlin 2.1
-###### MARLIN COLOR UI
-- [TMC2225](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/MKS_Robin_Nano_V1.3/Firmware/TMC2225)
-- [TMC2225 - Bltouch](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/MKS_Robin_Nano_V1.3/Firmware/TMC2225-BLTOUCH)
+###### Versione Marlin 2.1.x
+###### LVGL UI
+- [TMC2225](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/Firmware/MKS_Robin_Nano_V1.3/STOCK)
+- [TMC2225 - Bltouch](https://github.com/flyingbear-club-ita/flyingbear_ghost_marlin/tree/main/Firmware/MKS_Robin_Nano_V1.3/STOCK-BLTOUCH)
 
 ---
 
@@ -48,4 +61,4 @@ MKS Robin Nano-S V1.3
 Credits:
 - fork from [Marlin Firmware](https://github.com/MarlinFirmware/Marlin)
 - [Flyingbear Ghost Wiki](https://flyingbearghost.com)
-- [Flyingbear Ghost Club](https://discord.gg/p2gtrKm)
+- [Flyingbear Ghost Community Italia](https://discord.gg/p2gtrKm)
---
+++ Source code/Marlin/FlyingbearGhost_ConfigFile.h
@@ -0,0 +1,252 @@
+/**
+**********************************************************************************************
+*******************************                 **********************************************
+*******************************    REFERENCE    **********************************************
+*******************************                 **********************************************
+**********************************************************************************************
+
+
+!! ATTENTION !!
+
+
+--> DEFAUTL_ENVS (Please change this parameter inside platformio.ini)
+
+default_envs = mks_robin_nano_v1v2           - MKS Robin Nano 1.1 / MKS Robin Nano 1.2 / MKS Reborn V2.0          
+default_envs = mks_robin_nano_v1_3_f4        - MKs Robin Nano 1.3 / MKS Robin Nano 1.3s
+
+
+--> MOTHERBOARD (Please change this parameter inside Configuration.h)
+
+BOARD_MKS_ROBIN_NANO             - MKS Robin Nano 1.1 / MKS Robin Nano 1.2 / MKS Reborn V2.0
+BOARD_MKS_ROBIN_NANO_V1_3_F4     - MKs Robin Nano 1.3 / MKS Robin Nano 1.3s
+
+
+-->Mainboard
+
+NANO11_STOCK    -    MKS Robin Nano 1.1 (Reborn V2.0) - Driver 2x A4988 2x TMC2208    
+NANO11_2208     -    MKS Robin Nano 1.1 (Reborn V2.0) - Driver 4x TMC2208
+NANO12_STOCK    -    MKS Robin Nano 1.2 - Driver 2x A4988 2x TMC2208   
+NANO12_2208     -    MKS Robin Nano 1.2 - Driver 4x TMC2208 
+NANO13_STOCK    -    MKS Robin Nano 1.3 - Driver 2x A4988 2x TMC2208   
+NANO13_2208     -    MKS Robin Nano 1.3 - Driver 4x TMC2208 
+NANO13S_STOCK   -    MKS Robin Nano 1.3s - Driver 4x TMC2225
+
+
+
+**********************************************************************************************
+***********************************                 ******************************************
+***********************************  EDITABLE MENU  ******************************************
+***********************************                 ******************************************
+**********************************************************************************************
+*********   Here you will choose your card, your drivers and your personal settings   *******/
+
+// Choose the name from boards.h that matches your setup
+
+#ifndef USER_BUILD
+
+ #define NANO11_STOCK                                         //<------- Define your Mainboard
+
+ #define FILAMENT_RUNOUT_SENSOR                               //<------- Enable or disable filament runout sensor
+
+ #define DEFAULT_AXIS_STEPS_PER_UNIT   { 80, 80, 400, 400 }   //<------- Define your step/mm
+
+ #define PIDEMP                                               //<------- Define your PIDTEMP
+  #define DEFAULT_Kp  22.20
+  #define DEFAULT_Ki   1.08
+  #define DEFAULT_Kd 114.00
+  
+ #define PIDTEMPBED                                           //<------- Define your PIDTEMPBED
+  #define DEFAULT_bedKp 10.00
+  #define DEFAULT_bedKi .023
+  #define DEFAULT_bedKd 305.4
+
+  //#define FBGHOST_BLTOUCH                                   //<------- Define if you have Bltouch or not
+    #if ENABLED (FBGHOST_BLTOUCH)
+     #define NOZZLE_TO_PROBE_OFFSET { 0, 0, 0 }               //<------- Define your probe offset
+    #endif
+  
+ //#define TFT_CLASSIC_UI
+ //#define TFT_COLOR_UI                                      //<------- Define your printer UI
+ #define TFT_LVGL_UI
+
+#endif
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/**********************************************************************************************
+***********************************                 *******************************************
+***********************************   CONFIG ZONE   *******************************************
+***********************************                 *******************************************
+***********************************************************************************************
+*****************   If you not are an expert, stay away from here   **************************/
+
+#if ENABLED (NANO11_STOCK)
+  #define USER_X_DIR false
+  #define USER_X_TYPE TMC2208_STANDALONE
+
+  #define USER_Y_DIR false
+  #define USER_Y_TYPE TMC2208_STANDALONE
+
+  #define USER_Z_DIR false
+  #define USER_Z_TYPE A4988
+
+  #define USER_E0_DIR false
+  #define USER_E0_TYPE A4988
+
+  #define SERVO0_PIN                          PB2   // Enable BLTOUCH
+
+#endif  
+
+#if ENABLED (NANO11_2208)
+  #define USER_X_DIR true
+  #define USER_X_TYPE TMC2208_STANDALONE
+
+  #define USER_Y_DIR false
+  #define USER_Y_TYPE TMC2208_STANDALONE
+
+  #define USER_Z_DIR true
+  #define USER_Z_TYPE TMC2208_STANDALONE
+
+  #define USER_E0_DIR true
+  #define USER_E0_TYPE TMC2208_STANDALONE
+
+  #define SERVO0_PIN                          PB2   // Enable BLTOUCH
+
+#endif  
+
+#if ENABLED (NANO12_STOCK)
+  #define USER_X_DIR true
+  #define USER_X_TYPE TMC2208_STANDALONE
+
+  #define USER_Y_DIR false
+  #define USER_Y_TYPE TMC2208_STANDALONE
+
+  #define USER_Z_DIR false
+  #define USER_Z_TYPE A4988
+
+  #define USER_E0_DIR false
+  #define USER_E0_TYPE A4988
+
+  #define SERVO0_PIN                          PA8   // Enable BLTOUCH
+
+#endif  
+
+#if ENABLED (NANO12_2208)
+  #define USER_X_DIR true
+  #define USER_X_TYPE TMC2208_STANDALONE
+
+  #define USER_Y_DIR false
+  #define USER_Y_TYPE TMC2208_STANDALONE
+
+  #define USER_Z_DIR true
+  #define USER_Z_TYPE TMC2208_STANDALONE
+
+  #define USER_E0_DIR true
+  #define USER_E0_TYPE TMC2208_STANDALONE
+
+  #define SERVO0_PIN                          PA8   // Enable BLTOUCH
+
+#endif  
+
+#if ENABLED (NANO13_STOCK)
+  #define USER_X_DIR false
+  #define USER_X_TYPE TMC2208_STANDALONE
+
+  #define USER_Y_DIR false
+  #define USER_Y_TYPE TMC2208_STANDALONE
+
+  #define USER_Z_DIR false
+  #define USER_Z_TYPE A4988
+
+  #define USER_E0_DIR false
+  #define USER_E0_TYPE A4988
+
+  #define SERVO0_PIN                          PA8   // Enable BLTOUCH
+#endif  
+
+#if ENABLED (NANO13_2208)
+  #define USER_X_DIR false
+  #define USER_X_TYPE TMC2208_STANDALONE
+
+  #define USER_Y_DIR false
+  #define USER_Y_TYPE TMC2208_STANDALONE
+
+  #define USER_Z_DIR true
+  #define USER_Z_TYPE TMC2208_STANDALONE
+
+  #define USER_E0_DIR true
+  #define USER_E0_TYPE TMC2208_STANDALONE
+
+  #define SERVO0_PIN                          PA8   // Enable BLTOUCH
+
+#endif  
+
+#if ENABLED (NANO13S_STOCK)
+  #define USER_X_DIR false
+  #define USER_X_TYPE TMC2208_STANDALONE
+
+  #define USER_Y_DIR false
+  #define USER_Y_TYPE TMC2208_STANDALONE
+
+  #define USER_Z_DIR true
+  #define USER_Z_TYPE TMC2208_STANDALONE
+
+  #define USER_E0_DIR true
+  #define USER_E0_TYPE TMC2208_STANDALONE
+
+  #define SERVO0_PIN                          PA8   // Enable BLTOUCH
+
+#endif  
+
+
+#if ENABLED (FBGHOST_BLTOUCH)
+  #define BLTOUCH
+  #define USE_PROBE_FOR_Z_HOMING
+  #define Z_MIN_PROBE_PIN PC4
+  #define MULTIPLE_PROBING 2
+  #define EXTRA_PROBING    1
+  #define AUTO_BED_LEVELING_BILINEAR
+  #define Z_SAFE_HOMING
+
+#else
+  #define Z_MIN_PROBE_USES_Z_MIN_ENDSTOP_PIN
+  #define PROBE_MANUALLY
+
+#endif
+
+#if ENABLED (FILAMENT_RUNOUT_SENSOR)
+ #define FIL_RUNOUT_PIN           PA4
+  #if COUNT_ENABLED(NANO11_STOCK, NANO11_2208, NANO12_STOCK, NANO12_2208)
+    #define FIL_RUNOUT_STATE      LOW  
+   #else
+    #define FIL_RUNOUT_STATE      HIGH 
+  #endif
+#endif
+
+#if ANY(TFT_COLOR_UI,TFT_CLASSIC_UI)
+  #define DISPLAY_CHARSET_HD44780 JAPANESE
+  #define ADVANCED_PAUSE_FEATURE
+
+#else
+  #define DISPLAY_CHARSET_HD44780 WESTERN
+
+#endif
+
